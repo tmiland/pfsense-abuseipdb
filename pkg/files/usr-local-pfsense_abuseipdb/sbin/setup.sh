4c00dd0a5aa5a73b7817d76428f803fc6e61f288
Tommy Miland <kontakt@tmiland.com>
2026-09-08 01:44:31 +0200

    Fix service status command (is_service_running returns $rc var), bump 0.1.1

--- pkg/files/usr-local-pfsense_abuseipdb/sbin/setup.sh
+++ pkg/files/usr-local-pfsense_abuseipdb/sbin/setup.sh
@@ -20,7 +20,7 @@ $svc = array(
     'rcfile' => 'pfsense_abuseipdb',
     'description' => 'AbuseIPDB Suricata Watcher',
     'custom_php_service_status_command' =>
-        'exec("/usr/bin/pgrep -f pfsense_abuseipdb.sh", $o); return (count($o) > 1);'
+        'exec("/usr/bin/pgrep -f pfsense_abuseipdb.sh", $o); $rc = (count($o) > 1);'
 );
 $menu = array(
     'name' => 'AbuseIPDB',
